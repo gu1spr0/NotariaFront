3ee35e67873286c03d251b02cf3ea58ea6c369d4
Israel Guillermo <imamani@innovapplications.com>
2021-02-10 23:04:14 -0400

    Cambios en el menu

--- src/app/helpers/auth.guard.ts
+++ src/app/helpers/auth.guard.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
-        private authService: AuthService;
+        private authService: AuthService
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
